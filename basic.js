c55a913347e261fff9c1fbc1415c456f67e8ae38
Richard Rodger <richard@ricebridge.com>
2023-08-28 15:24:01 +0100

    builkd

--- basic.js
+++ basic.js
@@ -1,3 +1,12 @@
+const Assert = require('node:assert')
+
 const Jsonic = require('@jsonic/jsonic-next')
 
-console.log(Jsonic('a:1'))
+process.stdout.write('basic0: ')
+
+const s0 = 'a:1'
+const v0 = Jsonic(s0)
+const sv0 = JSON.stringify(v0)
+Assert(sv0 == '{"a":1}')
+
+console.log('OK', s0, '->', sv0)
--- csv.js
+++ csv.js
@@ -1,10 +1,22 @@
+const Assert = require('node:assert')
+
 const { Jsonic } = require('@jsonic/jsonic-next')
 const { Csv } = require('@jsonic/csv')
 
+process.stdout.write('csv0: ')
+
 const csv = Jsonic.make().use(Csv)
 
-console.log(csv(`
+const s0 = `
 a,b
 1,2
 3,4
-`))
+`
+
+const v0 = csv(s0)
+
+const sv0 = JSON.stringify(v0)
+Assert(sv0 == '[{"a":"1","b":"2"},{"a":"3","b":"4"}]')
+
+console.log('OK', s0.replace(/\n+/g,' % '), '->', sv0)
+
